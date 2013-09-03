710679ce7fbd4737c715d9a59155ceeca2c57adc
Eric Berg <eric@bergd.net>
2013-09-03 03:10:37 -0400

    Added catlang.py basic functions

---
+++ catlang.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+print("NANANANANANANANA CATLANG")
+
+stack = []
+toylang = {
+        'push': lambda:not_implemented,
+        'pop': lambda:not_implemented,
+        'peek': lambda:not_implemented,
+        'execute': lambda:not_implemented,
+        'stack': lambda:not_implemented,
+        '+': lambda:not_implemented,
+        '-': lambda:not_implemented,
+        '*': lambda:not_implemented,
+        '/': lambda:not_implemented,
+}
+lang = toylang
+
+def lookup(word):
+    pass
+
+def push(word):
+    pass
+
+def pop(word):
+    pass
+
+def peek(word):
+    pass
+
+def execute():
+    pass
+
+def parse_line(line):
+    pass
+
+def execute_words(words):
+    pass
+
+print("\ngoodbye cat says SIGTERM")
